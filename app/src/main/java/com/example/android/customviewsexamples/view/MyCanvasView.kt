45aff9510b807a7eb836e1cb105b4c1e8eec8014
Javier <javilena87@gmail.com>
2020-08-29 13:59:55 +0200

    Frames

--- app/src/main/java/com/example/android/customviewsexamples/view/MyCanvasView.kt
+++ app/src/main/java/com/example/android/customviewsexamples/view/MyCanvasView.kt
@@ -1,10 +1,7 @@
 package com.example.android.customviewsexamples.view
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -32,6 +29,8 @@ class MyCanvasView(context: Context) : View(context) {
 
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
+    private lateinit var frame: Rect
+
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
         color = drawColor
@@ -48,15 +47,21 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
 
+
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+
+        // Calculate a rectangular frame around the picture.
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas.drawRect(frame, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
