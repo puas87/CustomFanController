26090f9a24b269997326edd77f10aa8e4111b995
Javier <javilena87@gmail.com>
2020-08-29 12:41:48 +0200

    Initialize a Path Object

--- app/src/main/java/com/example/android/customviewsexamples/view/MyCanvasView.kt
+++ app/src/main/java/com/example/android/customviewsexamples/view/MyCanvasView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import com.example.android.customviewsexamples.R
@@ -18,6 +19,8 @@ class MyCanvasView(context: Context) : View(context) {
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
 
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private var path = Path()
 
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
