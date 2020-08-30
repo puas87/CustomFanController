f5b9dda15ab0671f9b76e5fdd4ca8a315bddb3e7
Javier <javilena87@gmail.com>
2020-08-30 19:09:08 +0200

    Create and Initialize a Paint and a Path Object

--- app/src/main/java/com/example/android/customviewsexamples/view/ClippedView.kt
+++ app/src/main/java/com/example/android/customviewsexamples/view/ClippedView.kt
@@ -1,12 +1,25 @@
 package com.example.android.customviewsexamples.view
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+import com.example.android.customviewsexamples.R
 
 class ClippedView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
+
 }
