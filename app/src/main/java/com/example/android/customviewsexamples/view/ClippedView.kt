66cc6793a42f9b519e3225bdc5306f916ac95f17
Javier <javilena87@gmail.com>
2020-08-30 19:53:05 +0200

    Create the drawClippedRectangle() Method

--- app/src/main/java/com/example/android/customviewsexamples/view/ClippedView.kt
+++ app/src/main/java/com/example/android/customviewsexamples/view/ClippedView.kt
@@ -2,6 +2,7 @@ package com.example.android.customviewsexamples.view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -59,24 +60,59 @@ class ClippedView @JvmOverloads constructor(
         // drawQuickRejectExample(canvas)
     }
 
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(clipRectLeft, clipRectTop, clipRectRight, clipRectBottom)
+
+        canvas.drawColor(Color.WHITE)
+
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft, clipRectTop,
+            clipRectRight, clipRectBottom, paint
+        )
+
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius, clipRectBottom - circleRadius,
+            circleRadius, paint
+        )
+        paint.color = Color.BLUE
+        // Align the RIGHT side of the text with the origin.
+        paint.textSize = textSize
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(
+            context.getString(R.string.clipping),
+            clipRectRight, textOffset, paint
+        )
+    }
+
     private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
     }
+
     private fun drawDifferenceClippingExample(canvas: Canvas) {
     }
+
     private fun drawCircularClippingExample(canvas: Canvas) {
     }
+
     private fun drawIntersectionClippingExample(canvas: Canvas) {
     }
+
     private fun drawCombinedClippingExample(canvas: Canvas) {
     }
+
     private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
     }
+
     private fun drawOutsideClippingExample(canvas: Canvas) {
     }
+
     private fun drawTranslatedTextExample(canvas: Canvas) {
     }
+
     private fun drawSkewedTextExample(canvas: Canvas) {
     }
+
     private fun drawQuickRejectExample(canvas: Canvas) {
     }
 
